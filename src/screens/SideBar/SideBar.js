651bb59a8546790841f4b7bdacc8ce73512ca5e8
David Aparicio <david@sirena.app>
2020-01-08 11:16:47 -0300

    added pagination with inifinite scroll and animated transitions

--- src/screens/SideBar/SideBar.js
+++ src/screens/SideBar/SideBar.js
@@ -1,30 +1,40 @@
 import React, { useEffect } from 'react'
 import { PropTypes as ptypes } from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
-//Lodash
 import _get from 'lodash/get'
+
 //Components
+import SideBarItem from '../../components/SideBarItem'
 import ListHeader from '../../components/ListHeader'
 import ListFooter from '../../components/ListFooter'
-import SideBarItem from '../../components/SideBarItem'
-
-//Material (Responsive)
+//Material UI
 import Drawer from '@material-ui/core/Drawer'
 import Hidden from '@material-ui/core/Hidden'
+//Features
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import InfiniteScroll from 'react-infinite-scroller'
 
 const genericImage = `https://logodownload.org/wp-content/uploads/2018/02/reddit-logo-16.png`
-
 const useStyles = makeStyles((theme) => ({
   container: {
     height: '100%',
     backgroundColor: '#37383a',
-    position: 'relative'
+    position: 'relative',
+    [theme.breakpoints.down('sm')]: {
+      width: 380
+    }
   },
   list: {
     display: 'flex',
     flexDirection: 'column',
     height: '100%',
     overflowY: 'auto'
+  },
+  empty: {
+    display: 'flex',
+    justifyContent: 'center',
+    color: 'white',
+    fontWeight: 700
   },
   drawer: {
     [theme.breakpoints.up('sm')]: {
@@ -39,11 +49,12 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 function SideBar({
-  list,
   getList,
+  list,
   selectPost,
-  selectedPost,
   removePost,
+  selectedPost,
+  next,
   resetList,
   toggleDrawer,
   mobileOpen
@@ -59,6 +70,10 @@ function SideBar({
   const handleClickEntry = (entry) => () => {
     if (entry) {
       selectPost(entry)
+      console.log('Que sucede: ', toggleDrawer)
+      if (toggleDrawer) {
+        toggleDrawer()
+      }
     }
   }
 
@@ -75,34 +90,62 @@ function SideBar({
     }
   }
 
+  const onLoadMore = () => {
+    getList(next)
+  }
+
   const drawer = (
     <div className={classes.container}>
       <div className={classes.list}>
         <ListHeader text={'Reddit Posts'} />
-        {list.map((item, index) => {
-          let imgUrl = _get(item, 'data.thumbnail', genericImage)
+        {list && list.length > 0 ? (
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={onLoadMore}
+            hasMore={true}
+            useWindow={false}
+            threshold={400}
+          >
+            <ReactCSSTransitionGroup
+              transitionName='course-item'
+              transitionLeave={true}
+              transitionAppear={true}
+              transitionAppearTimeout={2500}
+              transitionEnterTimeout={1700}
+              transitionLeaveTimeout={1000}
+              component='div'
+            >
+              {list &&
+                list.map((item, index) => {
+                  let imgUrl = _get(item, 'data.thumbnail', genericImage)
 
-          if (!imgUrl.includes('.')) {
-            imgUrl = genericImage
-          }
-          return (
-            <SideBarItem
-              key={item.data.id}
-              author={item.data.author}
-              created={item.data.created}
-              title={item.data.title}
-              num_comments={item.data.num_comments}
-              imgUrl={imgUrl}
-              handleClickEntry={handleClickEntry(item)}
-              handleRemoveEntry={handleRemoveEntry(item)}
-              highlight={
-                selectedPost ? selectedPost.data.id === item.data.id : false
-              }
-              readed={item.readed}
-            />
-          )
-        })}
-
+                  if (!imgUrl.includes('.')) {
+                    imgUrl = genericImage
+                  }
+                  return (
+                    <SideBarItem
+                      key={item.data.id}
+                      author={item.data.author}
+                      created={item.data.created}
+                      title={item.data.title}
+                      num_comments={item.data.num_comments}
+                      imgUrl={imgUrl}
+                      handleClickEntry={handleClickEntry(item)}
+                      handleRemoveEntry={handleRemoveEntry(item)}
+                      highlight={
+                        selectedPost
+                          ? selectedPost.data.id === item.data.id
+                          : false
+                      }
+                      readed={item.readed}
+                    />
+                  )
+                })}
+            </ReactCSSTransitionGroup>
+          </InfiniteScroll>
+        ) : (
+          <div className={classes.empty}>{'Empty :c'}</div>
+        )}
         <ListFooter handleDismissAll={removeAll} />
       </div>
     </div>
@@ -112,7 +155,6 @@ function SideBar({
     <nav className={classes.drawer} aria-label='mailbox folders'>
       <Hidden smUp implementation='css'>
         <Drawer
-          //container={container}
           variant='temporary'
           anchor={'left'}
           open={mobileOpen}
@@ -145,7 +187,6 @@ function SideBar({
 SideBar.propTypes = {
   list: ptypes.array,
   selectedPost: ptypes.object,
-  //Functions
   getList: ptypes.func.isRequired,
   selectPost: ptypes.func.isRequired,
   removePost: ptypes.func.isRequired
--- src/screens/SideBar/SideBarContainer.js
+++ src/screens/SideBar/SideBarContainer.js
@@ -3,14 +3,15 @@ import { getList, selectPost, removePost, resetList } from './actions'
 import SideBar from './SideBar'
 
 function mapStateToProps(state, ownProps) {
-  const { entries, isFetching, selectedPost } = state
+  const { entries, isFetching, selectedPost, after } = state
 
   return {
     list: entries,
     isFetching,
     selectedPost,
     toggleDrawer: ownProps.toggleDrawer || null,
-    mobileOpen: ownProps.mobileOpen || false
+    mobileOpen: ownProps.mobileOpen || false,
+    next: after
   }
 }
 
